99a6bb9826bedb5eee4ed5255d5a8ef66deb53e9
Vladimir Parkhomchuk <vovaparkhomchuk@Vladimirs-MacBook-Air.local>
2020-04-25 03:51:02 +0300

    Updates

---
+++ precache-manifest.71fd02d5c1e73ce2eefa08b7f915bed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8b41c1405c789b90be4713b69b49ff",
+    "url": "/vovid/index.html"
+  },
+  {
+    "revision": "f9ed19f6bdb7c4657c92",
+    "url": "/vovid/static/css/main.2e63f98a.chunk.css"
+  },
+  {
+    "revision": "90778a1c332cbed44213",
+    "url": "/vovid/static/js/2.bc92e5e7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vovid/static/js/2.bc92e5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ed19f6bdb7c4657c92",
+    "url": "/vovid/static/js/main.614723b8.chunk.js"
+  },
+  {
+    "revision": "35a67e4df668b6f82bed",
+    "url": "/vovid/static/js/runtime-main.f50e4e19.js"
+  },
+  {
+    "revision": "54845a77c6121fc1b7f573e956f162a9",
+    "url": "/vovid/static/media/1472.54845a77.gif"
+  }
+]);
